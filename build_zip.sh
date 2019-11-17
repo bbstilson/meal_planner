ad2df433104f7445a432dcdcb469e7ea6f413948
bbstilson <bbstilson@fastmail.com>
2019-11-17 11:29:18 -0800

    fix build script

--- build_zip.sh
+++ build_zip.sh
@@ -3,4 +3,4 @@
 cd package
 zip -r9 ${OLDPWD}/lambda.zip .
 cd $OLDPWD
-zip -g lambda.zip s3_util.py ses_util.py trello.py planner.py
+zip -g lambda.zip s3_util.py ses_util.py trello_util.py planner.py
